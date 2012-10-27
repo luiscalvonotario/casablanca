04305d65aa2257e96cb41f21a3e08ab0c2730fa9
Luis Calvo <luis.calvo.notario@gmail.com>
2012-10-27 10:06:40 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casablanca::Application.config.secret_token = 'f7e4dc24581e145d45653353562717f78fe9b4dd6b902f1cf95e2e9bf8ad72f51b90287f68c6fb4898fbcc1902efa8c44a731e0b27cefd6806620f5be248a702'
